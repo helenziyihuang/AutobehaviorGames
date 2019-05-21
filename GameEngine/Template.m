6251787d93986eb14ffe3ff7a40b027f813d8783
jproney <nigelbess@gmail.com>
2019-05-21 11:13:00 -0700

    removed game engine from the gitignore to prevent version dependency

---
+++ GameEngine/Template.m
@@ -0,0 +1,10 @@
+classdef Template < GameObject
+    methods (Access = public)
+        function obj = Awake(obj)
+            %Called at the beginning of the game
+        end
+        function obj = Update(obj)
+            %Called once per frame if this object is enabled
+        end
+    end
+end
