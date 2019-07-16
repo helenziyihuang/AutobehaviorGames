4340c8ef611460a421977b190fc3de847c46a668
nigelbess <nigelbess@gmail.com>
2019-07-16 16:57:02 -0700

    grated circle is now generated from png image. updated submod

--- Main Game/GratedCircle.m
+++ Main Game/GratedCircle.m
@@ -18,35 +18,7 @@ classdef GratedCircle < PhysicsObject
             obj.initialOffset = (windowSize(1)/2-obj.radius)/2;
         end
         function img = GenerateImage(obj)
-            gb = imadjust(obj.GenerateGabor(100, pi/4, 64*pi, 0, 1), [0 1], [.5-obj.contrast/2 .5 + obj.contrast/2], .2);
-            res = size(gb);
-            side = floor(obj.radius)*2;
-            gb = imresize(gb, side/res(1));
-
-            [x,y]=meshgrid(1:side, 1:side);
-            circleMask = (((x - side/2).^2 + (y - side/2).^2)>=obj.radius^2);
-            gb(circleMask) = .5;
-            img = gb;
-        end
-        function gb=GenerateGabor(obj,sigma,theta,lambda,psi,gamma)
-            %james's procedural gabor function
-            sigma_x = sigma;
-            sigma_y = sigma/gamma;
-
-            % Bounding box
-            nstds = 3;
-            xmax = max(abs(nstds*sigma_x*cos(theta)),abs(nstds*sigma_y*sin(theta)));
-            xmax = ceil(max(1,xmax));
-            ymax = max(abs(nstds*sigma_x*sin(theta)),abs(nstds*sigma_y*cos(theta)));
-            ymax = ceil(max(1,ymax));
-            xmin = -xmax; ymin = -ymax;
-            [x,y] = meshgrid(xmin:xmax,ymin:ymax);
-
-            % Rotation 
-            x_theta=x*cos(theta)+y*sin(theta);
-            y_theta=-x*sin(theta)+y*cos(theta);
-
-            gb= exp(-.5*(x_theta.^2/sigma_x^2+y_theta.^2/sigma_y^2)).*cos(2*pi/lambda*x_theta+psi);
+            img = obj.PngToImg('grating.png');
         end
         function obj = Reset(obj,side)
             obj.SetVelocity(0,1);
